70d3448859991ae50b74a6eaded057a10fc9bd5d
mariari-chickennuggets <mariajessicalaurel@gmail.com>
2025-03-25 19:20:03 +0800

    addedd files

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Routes, Route, Navigate } from "react-router-dom";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Navigate to="/login" />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/login" element={<Login />} />
+    </Routes>
+  );
+};
+
+export default App;
