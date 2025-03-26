7190631efc24f800991ab048fbb62ed2c4b382d1
Jessie J <mariajessicalaurel@gmail.com>
2025-03-26 16:58:52 +0800

    Sidebar (#3)
    Co-authored-by: cgcombate <cedriccombate00@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
+import Sidebar from "./pages/Sidebar";
 
 const App = () => {
   return (
@@ -8,8 +9,10 @@ const App = () => {
       <Route path="/" element={<Navigate to="/login" />} />
       <Route path="/signup" element={<Signup />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/sidebar" element={<Sidebar />} />
     </Routes>
   );
 };
 
 export default App;
+
