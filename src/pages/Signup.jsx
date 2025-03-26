28e60a271f28a6d1808bc0a4239c6383b20b1cff
cgcombate <cedriccombate00@gmail.com>
2025-03-26 09:05:45 +0800

    modified login and sign up

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -5,10 +5,14 @@ const Signup = () => {
   const [showPassword, setShowPassword] = useState(false);
 
 return (
-    <div className="flex min-h-screen flex-col items-center justify-center bg-gray-100  py-6 font-montserrat">
-        <img src="/logo.webp" alt="DICT Logo" className="w-64 mx-auto mb-4" />
+    <div className="flex min-h-screen flex-col items-center justify-center bg-gray-100 py-6 font-montserrat">
+        <img
+            src="/logo.webp"
+            alt="DICT Logo"
+            className="w-64 mx-auto mb-4 fixed top-6"
+        />
 
-        <div className="w-full max-w-md p-8 bg-white shadow-xl">
+        <div className="w-full max-w-md p-8 bg-white shadow-md mt-24">
             <h2 className="text-2xl font-bold text-center text-gray-900">Sign up</h2>
 
             <form className="mt-6">
@@ -52,16 +56,15 @@ return (
             </p>
         </div>
 
-        <footer className="w-full  text-gray-600 text-sm mt-10 py-4">
-        <div className="max-w-6xl mx-auto">
-          
-          <div className=" border-gray-700 mt-6 pt-4 text-center text-gray-600 text-xs">
-            <p>This website is for official use only. Unauthorized access is prohibited.</p>
-          </div>
-        </div>
-      </footer>
+        <footer className="w-full fixed bottom-6 bg-gray-100 text-gray-600 text-sm">
+            <div className="max-w-6xl mx-auto">
+                <div className="border-gray-700 mt-6 pt-6 text-center text-gray-600 text-xs">
+                    <p>This website is for official use only. Unauthorized access is prohibited.</p>
+                </div>
+            </div>
+        </footer>
     </div>
-  );
+);
 };
 
 
